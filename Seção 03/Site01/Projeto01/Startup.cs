674968476854320ac5ea4a9866d03d83d1cd3135
odair.nti@gmail.com <odair.nti@gmail.com>
2020-10-12 21:32:01 -0300

    curso final

--- Seção 03/Site01/Projeto01/Startup.cs
+++ Seção 03/Site01/Projeto01/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore.Sqlite;
 using Site01.Database;
 
 namespace Projeto01
@@ -13,23 +14,23 @@ namespace Projeto01
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            services.AddControllers(options => options.EnableEndpointRouting = false);
-            services.AddControllersWithViews(options => options.EnableEndpointRouting = false);
-            services.AddRazorPages().AddMvcOptions(options => options.EnableEndpointRouting = false);
+           // services.AddControllers(options => options.EnableEndpointRouting = false);
+           // services.AddControllersWithViews(options => options.EnableEndpointRouting = false);
+           // services.AddRazorPages().AddMvcOptions(options => options.EnableEndpointRouting = false);
 
 
+            //ef = Entity Framework
+            //para saber os acesso procurar no google por: provider ef
             services.AddDbContext<DatabaseContext>(options =>
             {
-                options.UseSqlServer("Server=(localDB)\\MSSQLLocalDB;Database=Site01;Integrated Security = True;");
+                //  options.UseSqlServer("Server=(localDB)\\MSSQLLocalDB;Database=Site01");
+                options.UseSqlite("Data Source = Database\\BDados.db");
             });
-
             services.AddDistributedMemoryCache();
             services.AddSession();
-
         }
-
-
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
